5f516c29d4290d812424d9f34f10935874c570ad
Lab41PaulM <paulm@lab41.org>
2014-03-26 16:49:07 -0700

    now producing simple edge lists

--- examples/congress_voting/parse_congress.py
+++ examples/congress_voting/parse_congress.py
@@ -22,8 +22,9 @@ import glob
 import csv
 import itertools
 import networkx as nx
-
-
+import operator
+import os
+import shutil
 
 def main(argv):
 
@@ -31,56 +32,107 @@ def main(argv):
     files_list = glob.glob("2014/*/*.json")
     #files_list = ['2014/h12/data.json']
 
-    link_data = {}
+    if os.path.exists("senate"):
+        shutil.rmtree("senate")
+    if os.path.exists("house"):
+        shutil.rmtree("house")
+
+    os.makedirs("senate")
+    os.makedirs("house")
+
+    senate_data = {}
+    house_data = {}
 
     for fname in files_list:
             with open(fname,'r') as inputfile:
                     data = json.load(inputfile)
+                   
+                    if data["chamber"] == "s":
+                        current_dict = senate_data
+                    else:
+                        current_dict = house_data
 
                     for vt in data['votes']:
                             
-                            node_ids = [n['id'] for n in data['votes'][vt]]
+                        node_ids = [n['id'] for n in data['votes'][vt]]
+                        
+                        pairs = itertools.permutations(node_ids,2)
+                        for rep0, rep1 in pairs:
+                            if (rep0, rep1) not in current_dict:
+                                current_dict[(rep0, rep1)] = 1
+                            else:
+                                current_dict[(rep0, rep1)] +=1
 
-                            pairs = itertools.permutations(node_ids,2)
-                            for rep0, rep1 in pairs:
-                                if (rep0, rep1) not in link_data:
-                                    link_data[(rep0, rep1)] = 1
-                                else:
-                                    link_data[(rep0, rep1)] +=1
+    max_senate_vote = max(senate_data.iteritems(), key=operator.itemgetter(1))[1]
+    max_house_vote = max(house_data.iteritems(), key=operator.itemgetter(1))[1]
+    
+    print max_senate_vote
+    print max_house_vote
+    threshold_senate = float(max_senate_vote) * .8
+    threshold_house =  float(max_house_vote) * .8
+
+    print "Senate Thres: {}".format(threshold_senate)
+    print "House Thres: {}".format(threshold_house)
+
+    write_edge_list(senate_data, threshold_senate, "edges_senate.txt")
+    write_edge_list(house_data, threshold_house, "edges_house.txt")
+
+    senators, reps = load_congress("legislators-current.csv")
+    create_graph(senate_data, senators, "senate.graphml")
+    create_graph(house_data, reps, "house.graphml")
+    
 
 
-    # Write Edge Data
-    with open('edges.csv','w') as edgesoutput:
-            csvwriter = csv.writer(edgesoutput,delimiter='\t',quotechar='"',quoting=csv.QUOTE_ALL)
-            for (rep0, rep1), count in link_data.iteritems():
-                row = [rep0,rep1,count]
-                csvwriter.writerow(row)
 
-
-    G = nx.Graph()
-    for(rep0, rep1), count in link_data.iteritems():
-        
-        G.add_edge(rep0.decode('utf-8'), rep1.decode('utf-8'), weight=count)
-
-    with open('legislators-current.csv', 'r') as f:
+def load_congress(congress_file):
+    
+    with open(congress_file, 'r') as f:
+    
         csvreader = csv.reader(f,delimiter=',',quotechar='"')
         #skip the headers
         next(csvreader, None)  # skip the headers
-       
+        senators = {}
+        reps = {}
         for row in csvreader:
-            node_id = row[20]
             
-            #if node_id has zero length, then it is not a senator, but a house member, whose id is the 17th row
-            if node_id is None or len(node_id) == 0:
-                node_id = row[17]
+            name = "{} {}".format(row[1],row[0]).decode('utf-8')
+            state = row[5]
+            party = row[6]
 
-            if node_id in G.node:
-                G.node[node_id]['name'] = "{} {}".format(row[1],row[0]).decode('utf-8')
-                G.node[node_id]['type'] = row[4]
-                G.node[node_id]['party'] = row[6]
-                G.node[node_id]['state'] = row[5]
-            
-    nx.write_graphml(G,'congress_graph.graphml')
+            if row[4] == "sen":
+                senators[row[20]] = (name, state, party) 
+            else:
+                reps[row[17]] = (name, state, party)
+
+    return senators, reps
+
+
+
+def create_graph(edge_dict, node_dict, out):
+
+    G = nx.Graph()
+    for(p0, p1), count in edge_dict.iteritems():
+        G.add_edge(p0.decode('utf-8'), p1.decode('utf-8'), weight=count)
+    
+    for node_id in G.nodes_iter():
+
+        if node_id in node_dict:
+            G.node[node_id]['name'] = node_dict[node_id][0] 
+            G.node[node_id]['state'] = node_dict[node_id][1]
+            G.node[node_id]['party'] = node_dict[node_id][2]
+        else:
+            print "Node Id: {} not found in node_dict".format(node_id)
+    nx.write_graphml(G,out)
+
+
+def write_edge_list(curr_dict, threshold, filename):
+    with open(filename, 'w') as edgesoutput:
+        csvwriter = csv.writer(edgesoutput,delimiter='\t')
+        for (p0, p1), count in curr_dict.iteritems():
+            if count > threshold:
+                row = [p0,p1]
+                csvwriter.writerow(row)
+
 
 
 if __name__ == "__main__":
