c181010a01da15e5de15543b0fd1fb8b36299517
Lab41PaulM <paulm@lab41.org>
2014-03-26 14:50:09 -0700

    allowing for senate and house

--- examples/congress_voting/parse_congress.py
+++ examples/congress_voting/parse_congress.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2013 In-Q-Tel, Inc/Lab41, All Rights Reserved.
+# Copyright (c) 2014 In-Q-Tel, Inc/Lab41, All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,53 +24,64 @@ import itertools
 import networkx as nx
 
 
-# get list of json files
-files_list = glob.glob("2014/*/*.json")
-#files_list = ['2014/h12/data.json']
 
-link_data = {}
+def main(argv):
 
-for fname in files_list:
-	with open(fname,'r') as inputfile:
-		data = json.load(inputfile)
+    # get list of json files
+    files_list = glob.glob("2014/*/*.json")
+    #files_list = ['2014/h12/data.json']
 
-		for vt in data['votes']:
-			
-			node_ids = [n['id'] for n in data['votes'][vt]]
+    link_data = {}
 
-			pairs = itertools.permutations(node_ids,2)
-			for rep0, rep1 in pairs:
-                            if (rep0, rep1) not in link_data:
-                                link_data[(rep0, rep1)] = 1
-                            else:
-                                link_data[(rep0, rep1)] +=1
+    for fname in files_list:
+            with open(fname,'r') as inputfile:
+                    data = json.load(inputfile)
 
+                    for vt in data['votes']:
+                            
+                            node_ids = [n['id'] for n in data['votes'][vt]]
 
-# Write Edge Data
-with open('edges.csv','w') as edgesoutput:
-	csvwriter = csv.writer(edgesoutput,delimiter='\t',quotechar='"',quoting=csv.QUOTE_ALL)
-	for (rep0, rep1), count in link_data.iteritems():
-	    row = [rep0,rep1,count]
-	    csvwriter.writerow(row)
+                            pairs = itertools.permutations(node_ids,2)
+                            for rep0, rep1 in pairs:
+                                if (rep0, rep1) not in link_data:
+                                    link_data[(rep0, rep1)] = 1
+                                else:
+                                    link_data[(rep0, rep1)] +=1
 
 
-G = nx.Graph()
-for(rep0, rep1), count in link_data.iteritems():
-    
-    G.add_edge(rep0.decode('utf-8'), rep1.decode('utf-8'), weight=count)
+    # Write Edge Data
+    with open('edges.csv','w') as edgesoutput:
+            csvwriter = csv.writer(edgesoutput,delimiter='\t',quotechar='"',quoting=csv.QUOTE_ALL)
+            for (rep0, rep1), count in link_data.iteritems():
+                row = [rep0,rep1,count]
+                csvwriter.writerow(row)
 
-with open('legislators-current.csv', 'r') as f:
-    csvreader = csv.reader(f,delimiter=',',quotechar='"')
-    #skip the headers
-    next(csvreader, None)  # skip the headers
-   
-    for row in csvreader:
-        node_id = row[17]
+
+    G = nx.Graph()
+    for(rep0, rep1), count in link_data.iteritems():
         
-        if node_id in G.node:
-            G.node[node_id]['name'] = "{} {}".format(row[1],row[0]).decode('utf-8')
-	    G.node[node_id]['party'] = row[6]
-	    G.node[node_id]['state'] = row[5]
+        G.add_edge(rep0.decode('utf-8'), rep1.decode('utf-8'), weight=count)
 
-nx.write_graphml(G,'congress_graph.graphml')
+    with open('legislators-current.csv', 'r') as f:
+        csvreader = csv.reader(f,delimiter=',',quotechar='"')
+        #skip the headers
+        next(csvreader, None)  # skip the headers
+       
+        for row in csvreader:
+            node_id = row[20]
+            
+            #if node_id has zero length, then it is not a senator, but a house member, whose id is the 17th row
+            if node_id is None or len(node_id) == 0:
+                node_id = row[17]
 
+            if node_id in G.node:
+                G.node[node_id]['name'] = "{} {}".format(row[1],row[0]).decode('utf-8')
+                G.node[node_id]['type'] = row[4]
+                G.node[node_id]['party'] = row[6]
+                G.node[node_id]['state'] = row[5]
+            
+    nx.write_graphml(G,'congress_graph.graphml')
+
+
+if __name__ == "__main__":
+        main(sys.argv[1:])
