b6075caa70d0340ac1993df5e786016a49d586d2
Lab41PaulM <paulm@lab41.org>
2014-05-16 10:41:42 -0700

    updates to test

--- src/metrics_experiment/goodness_indicators.py
+++ src/metrics_experiment/goodness_indicators.py
@@ -15,18 +15,22 @@ import networkx.utils.community_utils as cu
 import matplotlib.pyplot as plt
 
 def main(argv):
-   
-    #arg0 --> communities file
-    f_ground_truth_comms = argv[0]
-    f_edge_list = argv[1]
+  
+    if len(argv) != 2:
+        print "Args <communities_file> <graph_edge_list>"
+        sys.exit(0)
 
-    G = nx.read_edgelist(f_edge_list, nodetype=int)
-    ground_truth = cu.read_communities_by_community(f_ground_truth_comms, G)
 
-    #found_comms = nx.bigclam(G, detect_comm=5000)
-    found_comms = cu.read_communities_by_community("snap_cmtyvv.txt", G)
-    print "found size: {}".format(len(found_comms))
-    test = GoodnessIndicatorTests(G, ground_truth, found_comms)
+    G = nx.read_edgelist(argv[1], nodetype=int)
+
+    ground_truth = cu.read_communities_by_community(argv[0],  G)
+    
+    if ground_truth is None:
+        sys.exit(0)
+
+    
+    print "Total Number of Ground Truth Communities: {}".format(len(ground_truth))
+    test = GoodnessIndicatorTests(G, ground_truth)
     test.test_separability()
 
     
@@ -51,50 +55,40 @@ class GoodnessIndicatorTests:
     '''
 
 
-    def __init__(self, G, ground_truth, found_comms):
+    def __init__(self, G, ground_truth):
         self.G = G
         self.ground_truth = ground_truth
-        self.found_comms = found_comms
-        
-        print "Analyzing the found communities"
-        self.metrics_found = nx.get_community_metrics(found_comms, G)
-        print "Analyzing the ground truth"
         self.metrics_ground_truth = nx.get_community_metrics(ground_truth, G)
-       
-
+        
 
     def test_separability(self):
 
-        k = 100
-
-        #first we sort the ground truth based on separabiity, then plot
-        self.metrics_ground_truth.sort(key=lambda x: x.separability, reverse=True) 
+        k = min(100, len(self.ground_truth))
+        x_values = range(k)    
+        
         plt.subplot(111)
         
-        sorted_separability = [c.separability for c in self.metrics_ground_truth]
-        
-
-        k = min(k, len(self.metrics_found), len(self.ground_truth))
-
-
-        #then we get the found communities and sort by conductance 
-        self.metrics_found.sort(key=lambda x: x.conductance)
-        by_conductance = [list(t) for t in zip(*sorted( zip( [x.conductance for x in self.metrics_found[:k]], sorted_separability), key=lambda x: x[0]))]
-        by_tpr = [list(t) for t in zip(*sorted( zip( [x.tpr for x in self.metrics_found[:k]], sorted_separability), key=lambda x: x[0]))]
-        by_density = [list(t) for t in zip(*sorted( zip( [x.density for x in self.metrics_found[:k]], sorted_separability), key=lambda x: x[0]))]
-          
-       
-        #calculate the running average
-        
-        
-
-        
-        x_values = range(k)    
+        #first we sort the ground truth based on separabiity, then plot
+        self.metrics_ground_truth.sort(key=lambda x: x.separability, reverse=True) 
+        sorted_separability = ([c.separability for c in self.metrics_ground_truth])
         plt.plot(x_values, running_avg(sorted_separability[:k]))
-        plt.plot(x_values, running_avg(by_conductance[1][:k]))
-        plt.plot(x_values, running_avg(by_tpr[1][:k]))
-
-        plt.plot(x_values, running_avg(by_density[1][:k])) 
+        
+        #sort in place by conductance
+        self.metrics_ground_truth.sort(key=lambda x: x.conductance, reverse=True) 
+       
+        for m in self.metrics_ground_truth[:10]:
+            print m
+        
+        plt.plot(x_values, running_avg([x.separability for x in self.metrics_ground_truth][:k]))
+        
+        #sort in place by tpr
+        self.metrics_ground_truth.sort(key=lambda x: x.tpr, reverse=True) 
+        plt.plot(x_values, running_avg([x.separability for x in self.metrics_ground_truth][:k]))
+        
+        #sort in place by density
+        self.metrics_ground_truth.sort(key=lambda x: x.density, reverse=True) 
+        plt.plot(x_values, running_avg(x.separability for x in self.metrics_ground_truth][:k]))
+        
         plt.legend(['U', 'c', 'tpr', 'density'], loc='upper right')        
         plt.ylabel("Separability")
         plt.xlabel("Rank, k")
