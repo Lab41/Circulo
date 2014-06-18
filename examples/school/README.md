137e576842c358590ebd387b0af81e7a21248cd7
Robbie Ostrow <rostrow@iqt.org>
2014-06-18 13:14:04 -0700

    added script to automatically run the example

--- examples/school/README.md
+++ examples/school/README.md
@@ -2,6 +2,7 @@
 #Primary School - Cumulative Networks
 
 
+Run `python do_example.py` to automatically perform all steps outlined below.
 
 ###Data Prep
 
@@ -16,7 +17,7 @@
 
 	`python parse_school.py sp_data_school_day_*.gexf_`
 
--- There should now be two new .graphml files in the working directory.
+There should now be two new .graphml files in the working directory.
 	
 ## Exercise
 
@@ -24,18 +25,26 @@ __Requirements__
 
 - NetworkX from https://networkx.github.io
 - iGraph from http://igraph.org
+- Unfortunately, plotting also requires Cairo from http://cairographics.org, along with its Python bindings. (py2cairo). So far, I've been unsuccessful downloading these bindings on OSX, so this script has to be run on GNU/Linux. You can download Cairo on Ubuntu with `sudo apt-get install libcairo2-dev`
 
-
-
-To c
 
 ```
 import igraph as ig
 
-G = ig.Graph()
+g = ig.load("sp_data_school_day_1_g.graphml") # whichever file you would like
 
-G.Read_GraphML(...) #whichever file you would like 
+# Assigning colors to genders for plotting
+colorDict = {"M": "blue", "F": "pink", "Unknown": "black"}
+
+for vertex in g.vs:
+	# each vertex is labeled as its classname and colored as its gender.
+    vertex["label"] = vertex["classname"]
+    vertex["color"] = colorDict[vertex["gender"]]
 
 
+layout = g.layout("fr") # Fruchterman-Reingold layout
+
+# If Cairo is improperly installed, raises TypeError: plotting not available
+ig.plot(g, layout=layout) 
 ```
 
---
+++ examples/school/do_example.py
@@ -0,0 +1,65 @@
+from subprocess import call
+import os
+import igraph as ig
+
+import parse_school
+
+
+def download():
+	"""
+	Downloads graphs from http://www.sociopatterns.org/datasets/primary-school-cumulative-networks/
+	and saves them in a directory schoolData. If schoolData already exists, it is cleared.
+	"""
+	if os.path.exists("schoolData"):
+		os.rmdir("schoolData")
+	os.mkdir("schoolData")
+	os.chdir("schoolData")
+	try:
+		call(["curl", "-O", "http://www.sociopatterns.org/wp-content/uploads/2014/05/sp_data_school_day_[1-2]_g.gexf_.gz"])
+	except Exception as e:
+		print "cURL failed -- check if the site still has the graph data."
+		raise(e)
+	for filename in os.listdir("."):
+		call(["gunzip", filename])
+
+def prepare():
+	"""
+	Takes each file in .gexf format and converts it into the igraph-readable graphml.
+	"""
+	for filename in os.listdir("."):
+		parse_school.writeGraphML(filename)
+
+
+def example():
+	"""
+	Performs the example outlined in the README. Draws the graph of one dataset.
+	"""
+	g = ig.load("sp_data_school_day_1_g.graphml") # whichever file you would like
+
+	# Assigning colors to genders for plotting
+	colorDict = {"M": "blue", "F": "pink", "Unknown": "black"}
+
+	for vertex in g.vs:
+		# each vertex is labeled as its classname and colored as its gender.
+   		vertex["label"] = vertex["classname"]
+    	vertex["color"] = colorDict[vertex["gender"]]
+
+
+	layout = g.layout("fr") # Fruchterman-Reingold layout
+
+	# If Cairo is improperly installed, raises TypeError: plotting not available
+	try:
+		ig.plot(g, layout=layout)
+	except TypeError as e:
+		print "It looks like Cairo isn't properly installed. Refer to the wiki."
+		exit(1)
+
+def main():
+	download()
+	prepare()
+	example()
+
+	
+
+if __name__ == "__main__":
+	main()
