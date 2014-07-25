298f2920732cad9f721179266b049df56d303a64
Robbie Ostrow <rostrow@iqt.org>
2014-07-25 16:10:20 -0700

    Bookkeeping and adding placeholder READMEs

---
+++ data/README.md
@@ -0,0 +1,11 @@
+TODO: make this a comprehensive README describing the directory.
+
+In the meantime, here are some links with lots of graphs. Most of these sites also point you towards other resources.
+
+<http://nexus.igraph.org>
+
+<https://networkdata.ics.uci.edu/about.php>
+
+<http://www-personal.umich.edu/~mejn/netdata/>
+
+<http://snap.stanford.edu/data/>
--- data/run_template.py
+++ data/run_template.py
@@ -15,7 +15,7 @@ from circulo.download_utils import download_with_notes
 
 GRAPH_NAME = # Graph name goes here.
 DOWNLOAD_URL = # url goes here.
-
+GRAPH_TYPE = # extension goes here. Something like .net, .gml, .graphml...
 
 def __download__(data_dir):
     """
@@ -38,7 +38,7 @@ def get_graph():
     TEMPLATE COMMENT: Downloads and prepares a graph
     """
     data_dir = os.path.join(os.path.dirname(__file__), "data")
-    graph_path = os.path.join(data_dir, GRAPH_NAME + ".graphml")
+    graph_path = os.path.join(data_dir, GRAPH_NAME + GRAPH_TYPE)
 
     if not os.path.exists(graph_path):
         __download__(data_dir)
