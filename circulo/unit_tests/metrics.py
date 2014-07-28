c3cb3854ff1f2dac673c8d97d8b39a76fbacd7a7
Lab41PatrickC <patrickc@lab41.org>
2014-07-27 21:21:13 -0400

    Don't test metrics_with_subgraph by default.
    Signed-off-by: Lab41PaulM <paulm@lab41.org>

--- circulo/unit_tests/metrics.py
+++ circulo/unit_tests/metrics.py
@@ -14,9 +14,12 @@ class TestMetrics(unittest.TestCase):
                     [4,5,6,10,16],
                     [8,9,14,15,18,20,22,23,24,25,26,27,28,29,30,31,32,33]]
         self.cover=igraph.VertexCover(self.G, membership)
-        vcm = VertexCoverMetric()
-        metrics=vcm.run_analysis(self.cover, weights = None)
-        self.comm_metrics = metrics.comm_metrics
+
+        self.comm_metrics = None
+        if False:
+          vcm = VertexCoverMetric()
+          metrics=vcm.run_analysis(self.cover, weights = None)
+          self.comm_metrics = metrics.comm_metrics
 
     def test_density(self):
         truth = [.4181818, .6, .22875817]
@@ -25,8 +28,9 @@ class TestMetrics(unittest.TestCase):
         test  = [ s.density() for s in self.cover.subgraphs() ]
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.density for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.density for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
 
     def test_avgdegree(self):
@@ -37,8 +41,9 @@ class TestMetrics(unittest.TestCase):
         test  = [ mean(s.degree()) for s in self.cover.subgraphs() ]
         self.assertListAlmostEquals(truth, test, 2)
 
-        test = [a.degree_avg for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test = [a.degree_avg for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_FOMD(self):
         truth = [0.545454545, 0, 0.277777778]
@@ -46,8 +51,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.fraction_over_median_degree()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test = [a.fomd for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test = [a.fomd for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_expansion(self):
         truth = [1.272727, 0.8, 0.555556]
@@ -55,8 +61,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.expansion()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.degree_boundary_avg for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.degree_boundary_avg for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
 
     def test_cutratio(self):
@@ -65,8 +72,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.cut_ratio()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.cut_ratio for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.cut_ratio for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
 
     def test_conductance(self):
@@ -75,8 +83,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.conductance()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.conductance for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.conductance for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_normalizedcut(self):
         truth = [0.346236559, 0.277027027, 0.229166667]
@@ -84,8 +93,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.normalized_cut()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.normalized_cut for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.normalized_cut for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_TPR(self):
         truth = [0.9091,0.6, 0.9444]
@@ -94,8 +104,9 @@ class TestMetrics(unittest.TestCase):
                   for s in self.cover.subgraphs() ]
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.tpr[1] for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.tpr[1] for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_MaxODF(self):
         truth = [.5,0.3333333, 0.5 ]
@@ -103,8 +114,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.maximum_out_degree_fraction()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.odf_dict['max'] for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.odf_dict['max'] for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_avgODF(self):
         truth = [0.138131313, 0.233333333, 0.117592593]
@@ -112,16 +124,18 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.average_out_degree_fraction()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.odf_dict['average'] for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.odf_dict['average'] for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_FlakeODF(self):
         truth = [0,0,0]
         test  = self.cover.flake_out_degree_fraction()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.odf_dict['flake'] for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.odf_dict['flake'] for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
     def test_separability(self):
         truth = [1.6428571,1.5, 3.5]
@@ -129,8 +143,9 @@ class TestMetrics(unittest.TestCase):
         test  = self.cover.separability()
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.separability for a in self.comm_metrics]
-        self.assertListAlmostEquals(truth, test, 2)
+        if self.comm_metrics:
+          test  = [a.separability for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
 
 
     def test_localclusteringcoefficient(self):
@@ -142,7 +157,14 @@ class TestMetrics(unittest.TestCase):
                   for s in self.cover.subgraphs() ]
         self.assertListAlmostEquals(truth, test, 2)
 
-        test  = [a.clustering_coefficient for a in self.comm_metrics]
+        if self.comm_metrics:
+          test  = [a.clustering_coefficient for a in self.comm_metrics]
+          self.assertListAlmostEquals(truth, test, 2)
+
+    def test_cohesiveness(self):
+        truth = []
+
+        test  = [ s.cohesiveness() for s in self.cover.subgraphs() ]
         self.assertListAlmostEquals(truth, test, 2)
 
     def assertListAlmostEquals(self, a, b, places=None, msg=None):
