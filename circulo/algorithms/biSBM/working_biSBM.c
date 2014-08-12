761fc4f3e99d9ad1cb05acb159b11d98156e064d
Robbie Ostrow <rostrow@iqt.org>
2014-08-12 12:10:54 -0700

    structure in place, need to implement scoring

--- circulo/algorithms/biSBM/working_biSBM.c
+++ circulo/algorithms/biSBM/working_biSBM.c
@@ -5,7 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
+#include <stdbool.h>
 // TODO move to .h
 
 #include "working_biSBM.h"
@@ -94,39 +95,54 @@ int *initialize_groups(int a, int b, igraph_vector_bool_t *types){
 
 
 double score_partition(int *partition, igraph_t *graph){
+  printf("scoring...\n");
   return 0.0;
 }
 
-double make_best_switch(int *partition, int a, int b, igraph_vector_bool_t *types, igraph_t *graph, bool *used, int num_used){
-  int best_switch = -1;
-  double best_score = 0; 
-  for (int i = 0; i < igraph_vector_bool_size(types); i++){
-    if (used[i]){
+
+
+double swap_and_score(int *partition, int v, int a, int group, igraph_t *graph){
+  printf("%d, %d, %d, %d\n", v, a, group, partition[v]);
+  if ((partition[v] == group) || (partition[v] < a && group >= a) || (partition[v] >= a && group < a)){
+    return -1;
+  }
+
+  int temp = partition[v];
+  partition[v] = group;
+  double score = score_partition(partition, graph);
+  partition[v] = temp;
+  return score;
+}
+
+
+
+double make_best_switch(int *partition, int a, int b, igraph_vector_bool_t *types, igraph_t *graph, bool *used){
+  int best_switch_vertex = -1;
+  int best_switch_group = -1;
+  double best_switch_score = 0; 
+  for (int v = 0; v < igraph_vector_bool_size(types); v++){
+    if (used[v]){
       continue; // meh. don't do it this way. TODO
     }
-    if (partition[i] < a){
-      // TODO decompose
-      for (int group = 0; group < a; group++){
-        if (group != partition[i]){
-          //swap and score
-          score = score_partition();// TODO
-          if (score > best_score){
-            best_switch = i;
-            best_score = score;
-          }
-        }
+    double best_curr_score = -1;
+    int best_curr_group = -1;
+    for (int group = 0; group < a + b; group++){
+      double score = swap_and_score(partition, v, a, group, graph);
+      if (score > best_curr_score){
+        best_curr_score = score;
+        best_curr_group = group;
       }
     }
-    else {
-      for (int group = a; group < a + b; group++){
-        if (group != partition[i]){
-          //swap and score TODO same as above, decompose
-        }
-      }
+    if (best_switch_score > best_curr_score){
+      best_switch_vertex = v;
+      best_switch_group = best_curr_group;
+      best_switch_score = best_curr_score;
     }
   }
 
-  used[best_switch] = true;
+  partition[best_switch_vertex] = best_switch_group;
+  used[best_switch_vertex] = true;
+  return best_switch_score;
 }
 
 
@@ -138,28 +154,32 @@ double iterate_once(int *partition, int a, int b, igraph_vector_bool_t *types, i
   int num_used = 0;
 
   // initialize to 0
-  bool *used = calloc(sizeof(bool) * igraph_vector_bool_size(types)); // TODO: allocate on stack
+  bool *used = calloc(igraph_vector_bool_size(types), sizeof(bool)); // TODO: allocate on stack
 
   while (num_used < igraph_vector_bool_size(types)){
-    new_score = make_best_switch(working_partition, a, b, types, graph, used, num_used);
+    int new_score = make_best_switch(working_partition, a, b, types, graph, used);
     if (new_score > max_score){
       max_score = new_score;
       memcpy(partition, working_partition, sizeof(int) * igraph_vector_bool_size(types));
     }
     num_used++;
   }
-
-
-
+  return max_score;
 }
 
-double run_algorithm(int *partition, int a, igraph_vector_bool_t *types, igraph_t *graph, int max_iters){
+
+double run_algorithm(int *partition, int a, int b, igraph_vector_bool_t *types, igraph_t *graph, int max_iters){
   int *latest_grouping = malloc(sizeof(int) * igraph_vector_bool_size(types));
   memcpy(latest_grouping, partition, sizeof(int) * igraph_vector_bool_size(types));
   double score = score_partition(partition, graph);
   for (int i = 0; i < max_iters; i++){
-    double new_score = iterate_once(latest_grouping, a, types, graph);
-    if (new_score <= score) break;
+    printf("Performing iteration %d...\n", i);
+    double new_score = iterate_once(latest_grouping, a, b, types, graph);
+    printf("Score: %f\n", new_score);
+    if (new_score <= score) {
+      printf("Score has not improved. Algorithm has converged.\n");
+      break;
+    }
     score = new_score;
     memcpy(partition, latest_grouping, sizeof(int) * igraph_vector_bool_size(types));
   }
@@ -193,9 +213,13 @@ int main(int argc, char *argv[]) {
   }
   printf("Mapping successful.\n");
 
+  printf("Initializing to random groups...\n");
   int *partition = initialize_groups(a, b, &types);
+  printf("Initialization successful.\n");
 
+  printf("Running algorithm...\n");
   double best_score = run_algorithm(partition, a, b, &types, &graph, max_iters);
+  printf("Algorithm completed successfully.\n");
 
   printf("%f\n", best_score);
   // result is stored in partition
