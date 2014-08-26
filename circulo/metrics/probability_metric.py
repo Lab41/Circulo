768b5f86ff939f8c5a94041fd5cfaae481d45049
Robbie Ostrow <rostrow@iqt.org>
2014-08-26 14:45:44 -0700

    added probability_metric_score

--- circulo/metrics/probability_metric.py
+++ circulo/metrics/probability_metric.py
@@ -1,7 +1,15 @@
 import igraph as ig
-import itertools
 import statistics
 
+
+def probability_metric_score(G, clusters):
+    """
+    Returns the mean of all of the cluster's scores under the probability metric 
+    defined below. Contains much less information than probability_metric_graph,
+    but allows for a single number with which one can compare clustering algorithms.
+    """
+    l = [p[0] for p in probability_metric_graph(G, clusters)]
+    return statistics.mean(l);
 
 def probability_metric_graph(G, clusters):
     """
@@ -9,7 +17,21 @@ def probability_metric_graph(G, clusters):
     clusters. Returns a list of 3-tuples [(a, b, c),...] where a is the mean,
     b the standard deviation, and c the variance, indexed by cluster id. 
 
-    TODO: explain about/change default value of n. 
+    This metric measures how likely a particle placed on some vertex will stay within
+    the original community after n random steps, where n is the number of vertices in 
+    the community (or some other, better value for normalization). 
+
+    This returns a list [(mean, variance)_0, (mean, variance)_1..., (mean, variance)_n]
+    where the statistics within each cluster are represented by its index in the list.
+
+    A high mean suggests strong community structure, but a high variance suggests that
+    a few objects in the community might be outliers and not necessarily belong.
+    
+    Currently, we use the size of the community as the number of steps taken, but this
+    isn't based on much theory. Some analysis is necessary to find a better choice for n.
+
+    If you want one "score" to score the clustering instead of individual communities, 
+    call probability_metric_score instead.
     """
     fullStats = []
     for cluster in clusters:
@@ -26,11 +48,12 @@ def probability_metric_cluster(G, members):
     TODO: testing, to find out whether this is a legitimate normalization.
     """
     nMembers = len(members) # figure out if this is a good normalization
+    # numVertices = G.vcount();
+    # normalization = nMembers / numVertices 
     data = [p_in_after_n(G, v, nMembers, members) for v in members]
-    mean = statistics.mean(data)
-    stddev = statistics.pstdev(data, mu=mean)
+    mean = statistics.mean(data) # could divide mean by normalization
     var = statistics.pvariance(data, mu=mean)
-    return mean, stddev, var
+    return mean, var
 
 
 def p_in_after_n(G, v, n, comm):
@@ -40,6 +63,8 @@ def p_in_after_n(G, v, n, comm):
     n step random walk, beginning from v. At each step,
     the particle is equally likely to travel to any
     of its neighbors.
+
+    TODO: use weights
     """
     return p_in_after_n_r_cached(G, v, n, set(comm), {})
 
@@ -71,10 +96,3 @@ def p_in_after_n_r_cached(G, v, n, comm, cache):
         totalP += 1/numNeighbors * pGivenNeighbor
     cache[(v, n)] = totalP
     return totalP
-
-
-if __name__ == "__main__":
-    g = ig.load("netscience_single_component.gml")
-    clust = g.community_edge_betweenness().as_clustering()
-    print(probability_metric_graph(g, clust))
-   # print probability_metric(g, clust, 3)
