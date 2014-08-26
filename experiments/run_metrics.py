3a0b8a79db8e20ca195adfd4630911dcd0025b3a
Lab41PaulM <paulm@lab41.org>
2014-08-26 10:33:46 -0700

    Updated metrics exec in prep for json

--- experiments/run_metrics.py
+++ experiments/run_metrics.py
@@ -4,21 +4,67 @@ from sklearn import metrics as skmetrics
 import numpy as np
 import pickle
 import argparse
+import os
+import glob
 
-parser = argparse.ArgumentParser(description='Compute metrics for given cover.')
-parser.add_argument('cover', type=str, nargs=1, 
-                          help='pickle file for first cover')
-args = parser.parse_args()
 
-results = pickle.load(open(args.cover[0], 'rb'))
 
-# Compute cover metrics
-print('Calculating cover metrics... ')
-for cover in results['vc']:
-    weights = 'weight' if cover.graph.is_weighted() else None
-    cover.compute_metrics(weights=weights) 
+def main():
+    parser = argparse.ArgumentParser(description='Compute metrics for given cover.')
+    parser.add_argument('path', type=str, nargs=1,
+                              help='pickle file or directory containing pickles for first cover')
+    parser.add_argument('output_dir', type=str, default=['metrics_output'], nargs=1, help='Output directory for metrics')
 
-# Output results to pickle
-output = open(args.cover[0], 'wb')
-pickle.dump(results, output)
-output.close()
+    args = parser.parse_args()
+
+    out_dir = args.output_dir[0]
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    for path in args.path:
+
+        if not os.path.exists(path):
+            print("Path \"{}\" does not exist".format(path))
+            continue
+
+        if os.path.isdir(path):
+            for f in glob.glob(os.path.join(path,'*.pickle')):
+                analyze_pickle(f, out_dir)
+        else:
+            print("Found File")
+            continue
+
+
+
+def analyze_pickle(pickle_file, output_dir):
+
+    results = pickle.load(open(pickle_file, 'rb'))
+
+    # Compute cover metrics
+    print('Calculating cover metrics... ')
+    for cover in results['vc']:
+        weights = 'weight' if cover.graph.is_weighted() else None
+        cover.compute_metrics(weights=weights)
+
+        d = {
+            "name" : results['vc_name'],
+            "elapsed" :results['elapsed'],
+            "membership" : cover.membership,
+            "metrics": cover.metrics
+            }
+
+        print(d)
+
+
+
+
+    #Output results to pickle
+    #output = open(args.cover[0], 'wb')
+    #pickle.dump(results, output)
+    #output.close()
+
+
+
+if __name__ == "__main__":
+    main()
