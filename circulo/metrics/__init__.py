9193b52ac11bbdcd08b82724e6abff36dcd11793
ChrisM <ChrisM@lab41.org>
2014-08-26 11:04:43 -0700

    Fixed import statement

--- circulo/metrics/__init__.py
+++ circulo/metrics/__init__.py
@@ -1,3 +1,4 @@
-from circulo.metrics import cover, graph
+import circulo.metrics.cover
+import circulo.metrics.graph
 from circulo.metrics.metrics_non_subgraph import VertexCoverMetric
 from circulo.metrics.omega import omega_index
