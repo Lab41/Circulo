5b97341596118893d02b6601105e706c28ee4666
Lab41PaulM <paulm@lab41.org>
2014-10-24 10:36:23 -0700

    updates to data etl

--- circulo/data/scotus/run.py
+++ circulo/data/scotus/run.py
@@ -1,91 +1,47 @@
 import igraph
-from igraph import VertexClustering
 import os
-import sys
-import urllib.request
 import csv
-from circulo.utils.downloader import download_with_notes
+from circulo.data.databot import *
 
 DATA_DOWNLOAD_URL = 'http://jhfowler.ucsd.edu/data/judicial.csv'
-VERTEX_DATA_FILE = 'SCOTUS_DATA.csv'
+VERTEX_DATA_FILE = 'judicial.csv'
 
 EDGE_DOWNLOAD_URL = 'http://jhfowler.ucsd.edu/data/allcites.txt'
-EDGE_DATA_FILE = 'SCOTUS_EDGES.edgelist'
-
-GRAPH_NAME = 'SCOTUS'
-GRAPH_TYPE = '.graphml'
-
-def __download__(data_dir):
-    """
-    TEMPLATE COMMENT: downloads the graph from DOWNLOAD_URL into data_dir/GRAPH_NAME
-    """
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
-    download_with_notes(DATA_DOWNLOAD_URL, VERTEX_DATA_FILE, data_dir)
-    download_with_notes(EDGE_DOWNLOAD_URL, EDGE_DATA_FILE, data_dir)
-
-def __prepare__(data_dir):
-    """
-    TEMPLATE COMMENT: prepare the data into graphml format.
-    """
-
-    vertex_filename = os.path.join(data_dir, VERTEX_DATA_FILE)
-    edge_filename = os.path.join(data_dir, EDGE_DATA_FILE)
-
-    g = igraph.load(edge_filename)
-
-    vertex_file = open(vertex_filename)
-    reader = csv.DictReader(vertex_file)
-
-    for case in reader:
-        caseid = int(case['caseid'])
-        v = g.vs[caseid]
-
-        v['caseid']  = case['caseid']
-        v['usid']    = case['usid']
-        v['parties'] = case['parties']
-        v['year']    = case['year']
-
-    vertex_file.close()
-
-    # Case IDs are 1-indexed, so we delete the 0th vertex as it was extraneous.
-    g.delete_vertices([0])
-
-    graph_file = os.path.join(data_dir, GRAPH_NAME + GRAPH_TYPE)
-    g.write_graphml(graph_file)
-
-def get_graph():
-    """
-    TEMPLATE COMMENT: Downloads and prepares a graph
-    """
-    data_dir = os.path.join(os.path.dirname(__file__), "data")
-    graph_path = os.path.join(data_dir, GRAPH_NAME + GRAPH_TYPE)
-
-    if not os.path.exists(graph_path):
-        __download__(data_dir)
-        __prepare__(data_dir)
-    else:
-        print("\t+",graph_path, "already exists. Using old file.")
-
-    return igraph.load(graph_path)
+EDGE_DATA_FILE = 'allcites.txt'
 
 
-def get_ground_truth(G=None):
-    """
-    TEMPLATE COMMENT: returns a VertexClustering object of the
-    ground truth of the graph G.
-    """
-    raise(NotImplementedError)
+class SCOTUSData(CirculoData):
 
-    # if G is None:
-    #     G = get_graph()
+    def __download__(self):
+        self.download_with_notes(DATA_DOWNLOAD_URL)
+        self.download_with_notes(EDGE_DOWNLOAD_URL)
 
-    # return # VertexClustering(G, membership)
+    def __prepare__(self):
+        vertex_filename = os.path.join(self.raw_data_path, VERTEX_DATA_FILE)
+        edge_filename = os.path.join(self.raw_data_path, EDGE_DATA_FILE)
 
+        g = igraph.load(edge_filename)
+
+        vertex_file = open(vertex_filename)
+        reader = csv.DictReader(vertex_file)
+
+        for case in reader:
+            caseid = int(case['caseid'])
+            v = g.vs[caseid]
+
+            v['caseid']  = case['caseid']
+            v['usid']    = case['usid']
+            v['parties'] = case['parties']
+            v['year']    = case['year']
+
+        vertex_file.close()
+
+        # Case IDs are 1-indexed, so we delete the 0th vertex as it was extraneous.
+        g.delete_vertices([0])
+        g.write_graphml(self.graph_path)
 
 def main():
-    G = get_graph()
-#    get_ground_truth(G)
+    SCOTUSData("scotus").get_graph()
 
 if __name__ == "__main__":
     main()
--- circulo/data/southernwomen/run.py
+++ circulo/data/southernwomen/run.py
@@ -1,58 +1,26 @@
-import igraph
-from igraph import VertexClustering
 import os
-import sys
-import urllib.request
-from circulo.utils.downloader import download_with_notes
+import shutil
+from circulo.data.databot import *
 
-
-ZIP_NAME = "Davis-GraphML.zip"
 DOWNLOAD_URL = "http://nexus.igraph.org/api/dataset?id=23&format=GraphML"
+DATA_ID = "southernwomen"
+DOWNLOAD_FILE="Davis.GraphML"
 
-GRAPH_NAME = "davis"
-GRAPH_TYPE = ".graphml"
+class SouthernWomenData(CirculoData):
 
-def __download__(data_dir):
-    """
-    TEMPLATE COMMENT: downloads the graph from DOWNLOAD_URL into data_dir/GRAPH_NAME
-    """
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
-    download_with_notes(DOWNLOAD_URL, ZIP_NAME, data_dir, progressbar=False)
+    def __download__(self):
+        self.download_with_notes(DOWNLOAD_URL, progressbar=False, download_file=DOWNLOAD_FILE)
 
-def __prepare__(data_dir):
-    """
-    TEMPLATE COMMENT: prepare the data into graphml format.
-    """
-    pass
+    def __prepare__(self):
+        shutil.copyfile(os.path.join(self.raw_data_path, DOWNLOAD_FILE), self.graph_path)
 
-def get_graph():
-    """
-    TEMPLATE COMMENT: Downloads and prepares a graph
-    """
-    data_dir = os.path.join(os.path.dirname(__file__), "data")
-    graph_path = os.path.join(data_dir, GRAPH_NAME + GRAPH_TYPE)
-
-    if not os.path.exists(graph_path):
-        __download__(data_dir)
-        __prepare__(data_dir)
-    else:
-        print(graph_path, "already exists. Using old file.")
-
-    return igraph.load(graph_path)
-
-
-def get_ground_truth(G=None):
-    """
-    TEMPLATE COMMENT: returns a VertexClustering object of the
-    ground truth of the graph G.
-    """
-    raise(NotImplementedError)
+    def get_ground_truth(G=None):
+        print("Ground truth is not available for this dataset")
+        raise(NotImplementedError)
 
 
 def main():
-    G = get_graph()
-#    get_ground_truth(G)
+    SouthernWomenData(DATA_ID).get_graph()
 
 if __name__ == "__main__":
     main()
