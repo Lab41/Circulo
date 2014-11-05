c5909dfa6c731d5ed2e18cc5c6d1473cf56f4b5a
Lab41PaulM <paulm@lab41.org>
2014-11-05 14:45:29 -0800

    fixed space issue in metrics result

--- circulo/utils/general.py
+++ circulo/utils/general.py
@@ -7,14 +7,14 @@ def aggregate(array, prefix="",axis=0):
     stats = describe(array, axis)
 
     return  {
-        prefix+' Size':int(stats[0]),
-        prefix+' Min':float(stats[1][0]),
-        prefix+' Max':float(stats[1][1]),
-        prefix+' Mean':float(stats[2]),
-        prefix+' Unbiased Variance':float(stats[3]),
-        prefix+' Biased Skewness':float(stats[4]),
-        prefix+' Biased Kurtosis':float(stats[5]),
-        prefix+' Median':float(median(array,axis))
+        prefix+'Size':int(stats[0]),
+        prefix+'Min':float(stats[1][0]),
+        prefix+'Max':float(stats[1][1]),
+        prefix+'Mean':float(stats[2]),
+        prefix+'Unbiased Variance':float(stats[3]),
+        prefix+'Biased Skewness':float(stats[4]),
+        prefix+'Biased Kurtosis':float(stats[5]),
+        prefix+'Median':float(median(array,axis))
             }
 
 
