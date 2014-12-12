7c4b5531fca360edd1c8d16d3202a5d5f5a29334
Yonas Tesfaye <ytesfaye@l41-lt-3.local>
2014-12-12 14:16:25 -0800

    Reflect Changes in Ground Truth Location
    General cleanup + reflecting treating ground truth as just another
    algorithm (instead of in a separate directory)

--- experiments/create_graphml.py
+++ experiments/create_graphml.py
@@ -1,25 +1,25 @@
-# Now cluster the clusters
-# from circulo import metrics
-# from sklearn import metrics as skmetrics
-# import numpy as np
-# import pickle
-# import argparse
-# import os
-# import glob
-# import json
-# from igraph import VertexCover
-# import importlib
-# import circulo.metrics.cover
+#!/usr/bin/env python
+#
+# Copyright (c) 2014 In-Q-Tel, Inc/Lab41, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import multiprocessing
 import time
 import signal
 import os
 import errno
 import traceback
-# import sys
 from collections import namedtuple
-# import inspect
-# from circulo.data.databot import CirculoData
 import glob
 import sys
 import networkx as nx
@@ -32,14 +32,13 @@ import json
 import datetime
 import multiprocessing
 
+Worker = namedtuple('Worker', 'json_path raw_graph_path output_path timeout')
 
-Worker = namedtuple('Worker', 'json_path ground_truth_path output_path timeout')
 
 def main():
-
     parser = argparse.ArgumentParser(description='Compute metrics for given cover.')
     parser.add_argument('input_path', type=str, help='file or directory containing results')
-    parser.add_argument('ground_truth_path', type=str, help='file or directory containing ground truth')
+    parser.add_argument('raw_graph_path', type=str, help='File or directory graphml files [typically circulo/data/GRAPHS/]')
     parser.add_argument('output_path', type=str, help='output directory to write metric files')
     parser.add_argument('--workers', type=int, default=multiprocessing.cpu_count(), help='Number of workers to process (DEFAULT: number of processors)')
     parser.add_argument('--timeout', type=int, default=3600, help="timeout for a work item in seconds (DEFAULT: 3600)")
@@ -53,7 +52,6 @@ def main():
         os.makedirs(args.output_path)
 
     workers = []
-
     json_groups = {}
     json_files = glob.glob(os.path.join(args.input_path, '*.json'))
     for json_file in json_files:
@@ -64,19 +62,15 @@ def main():
         else:
             json_groups[prefix] = [json_file]
 
-    ground_truth_files = glob.glob(os.path.join(args.ground_truth_path, '*.graphml'))
+    raw_graph_files = glob.glob(os.path.join(args.raw_graph_path, '*.graphml'))
     for prefix in json_groups:
-        ground_truth_file_path = None
-        for ground_truth_file in ground_truth_files:
-            #print(ground_truth_file, os.path.basename(ground_truth_file), prefix)
-            if os.path.basename(ground_truth_file).startswith(os.path.basename(prefix)):
-                ground_truth_file_path = ground_truth_file
-        workers.append(Worker(json_groups[prefix], ground_truth_file_path, args.output_path, args.timeout))
-    # if os.path.isdir(args.input_path):
-    #     for f in glob.glob(os.path.join(args.input_path, '*.json')):
-    #         workers.append(Worker(f, args.output_path, args.timeout))
-    # else:
-    #     workers.append(Worker(args.input_path, args.output_path, args.timeout))
+        raw_graph_file_path = None
+        for raw_graph_file in raw_graph_files:
+            if os.path.basename(raw_graph_file).startswith(os.path.basename(prefix)):
+                raw_graph_file_path = raw_graph_file
+        workers.append(Worker(json_groups[prefix], raw_graph_file_path, args.output_path, args.timeout))
+
+    workers.append(Worker(args.input_path, args.output_path, args.timeout))
 
     if args.workers is not None:
         pool = multiprocessing.Pool(processes = args.workers)
@@ -88,8 +82,10 @@ def main():
     pool.close()
     pool.join()
 
+
 class TimeoutError(Exception):
     pass
+
 
 def __handle_timeout(signum, frame):
     raise TimeoutError(os.strerror(errno.ETIME))
@@ -98,34 +94,31 @@ def __handle_timeout(signum, frame):
 def __get_least_frequent_community(community_array, community_counts):
     counts = []
     for community in community_array:
-        counts.append( (community_counts[community], community))
-
+        counts.append((community_counts[community], community))
     counts.sort()
 
-    #print(community_array, counts)
     for i, (count,community) in enumerate(counts):
-        #print (i, len(counts), count, counts)
         if count != 1 or i == len(counts):
-            #print( "Returning %s for %s"%(community, community_array) )
             return community
 
-def analyze_json(worker):
 
+def analyze_json(worker):
+    """
+    Take in a set of json community detection results files and a graphml file representing the raw graph and output a
+    graphml file that contains, as attributes, the results of the aglortihms
+
+    Args:
+    worker: Named tuple of json_path raw_graph_path output_path timeout
+    """
     signal.signal(signal.SIGALRM, __handle_timeout)
     signal.setitimer(signal.ITIMER_REAL, worker.timeout)
-    t0 = time.time()
 
-
-  
-
-    print('Loading raw Graphml file truth file: %s'%worker.ground_truth_path)
-    if worker.ground_truth_path is not None:
-        G = nx.read_graphml(worker.ground_truth_path)
+    print('Loading raw Graphml file truth file: %s'%worker.raw_graph_path)
+    if worker.raw_graph_path is not None:
+        G = nx.read_graphml(worker.raw_graph_path)
     else:
         print("ERROR: Not able to load graph")
-        return 
-        ground_truth = None
-
+        return
 
     try:
         for json_path in worker.json_path:
@@ -152,20 +145,11 @@ def analyze_json(worker):
                     except IndexError:
                         community_array= []
 
+                    # TODO: We have to pick single community when there are multiple (is it right to pick least freq?)
                     least_frequent_community = __get_least_frequent_community(community_array, community_counts)
-                    if least_frequent_community == None:
-                        #print('Least Frequent community is null: %s'%community_array)
+                    if least_frequent_community is None:
                         least_frequent_community = -1
                     G.node[node][algo_name] = str(least_frequent_community)
-                  
-
-                # for i, node in enumerate(data['membership']):
-                #     node_name = "n%d"%i
-                #     #print(i, node,node_name, algo_name)
-                #     # get least frequent 
-                #     G.node[node_name][algo_name] = __get_least_frequent_community(node, community_counts)
-            
-                
 
     except TimeoutError as t:
         print("\t+Timeout ERROR: was analyzing: ", data['job_name'])
@@ -176,11 +160,9 @@ def analyze_json(worker):
         traceback.print_exc(file=sys.stdout)
         return
 
-    #(name, algorithm) = data[0]['job_name'].split('--')[:2]
     graphml_file_output = os.path.join(worker.output_path, "%s.graphml"% name)
     print("Writing Graph: %s"%graphml_file_output )
     nx.write_graphml(G, graphml_file_output)
-
 
 
 if __name__ == "__main__":
