d8430682a173cc37afd20982df6584d7f9c652d6
Yonas Tesfaye <ytesfaye@l41-lt-3.local>
2014-12-12 14:19:04 -0800

    Cleanup run_comparison
    Cleanup to treat ground truth as just another algorithm. Also removing
    unused code and adding some comments

--- experiments/run_comparison.py
+++ experiments/run_comparison.py
@@ -1,4 +1,18 @@
-# Now cluster the clusters
+#!/usr/bin/env python
+#
+# Copyright (c) 2014 In-Q-Tel, Inc/Lab41, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from circulo import metrics
 from sklearn import metrics as skmetrics
 import numpy as np
@@ -21,14 +35,12 @@ from collections import namedtuple
 import inspect
 from circulo.data.databot import CirculoData
 
+Worker = namedtuple('Worker', 'json_path output_path timeout')
 
-Worker = namedtuple('Worker', 'json_path ground_truth_path output_path timeout')
 
 def main():
-
-    parser = argparse.ArgumentParser(description='Compute metrics for given cover.')
+    parser = argparse.ArgumentParser(description='Compute the relative distance of various results from each other.')
     parser.add_argument('input_path', type=str, help='file or directory containing results')
-    parser.add_argument('ground_truth_path', type=str, help='file or directory containing ground truth')
     parser.add_argument('output_path', type=str, help='output directory to write metric files')
     parser.add_argument('--workers', type=int, default=multiprocessing.cpu_count(), help='Number of workers to process (DEFAULT: number of processors)')
     parser.add_argument('--timeout', type=int, default=3600, help="timeout for a work item in seconds (DEFAULT: 3600)")
@@ -43,35 +55,26 @@ def main():
 
     workers = []
 
+    # Group results file by dataset (treat ground truth as just another dataset)
     json_groups = {}
     json_files = glob.glob(os.path.join(args.input_path, '*.json'))
     for json_file in json_files:
         prefix = json_file[:json_file.find('--')]
-
         if prefix in json_groups:
             json_groups[prefix].append(json_file)
         else:
             json_groups[prefix] = [json_file]
 
-    ground_truth_files = glob.glob(os.path.join(args.ground_truth_path, '*groundtruth.json'))
+    # Associate ground truth files with the dataset they come from
     for prefix in json_groups:
-        ground_truth_file_path = None
-        for ground_truth_file in ground_truth_files:
-            #print(ground_truth_file, os.path.basename(ground_truth_file), prefix)
-            if os.path.basename(ground_truth_file).startswith(os.path.basename(prefix)):
-                ground_truth_file_path = ground_truth_file
-        workers.append(Worker(json_groups[prefix], ground_truth_file_path, args.output_path, args.timeout))
-    # if os.path.isdir(args.input_path):
-    #     for f in glob.glob(os.path.join(args.input_path, '*.json')):
-    #         workers.append(Worker(f, args.output_path, args.timeout))
-    # else:
-    #     workers.append(Worker(args.input_path, args.output_path, args.timeout))
+         workers.append(Worker(json_groups[prefix], args.output_path, args.timeout))
 
     if args.workers is not None:
-        pool = multiprocessing.Pool(processes = args.workers)
+        pool = multiprocessing.Pool(processes=args.workers)
     else:
         pool = multiprocessing.Pool()
 
+    # Run processing
     r = pool.map_async(analyze_json, workers)
     r.get() #must call in order to get error from inside the child processes
     pool.close()
@@ -84,81 +87,44 @@ def __handle_timeout(signum, frame):
     raise TimeoutError(os.strerror(errno.ETIME))
 
 def analyze_json(worker):
+    """
+    For a given set of json files (covering a single dataset) compare each results set to every other result set.
 
+    Args:
+       worker: named tuple containing <list of json files to process> <output direcotry> timeout
+
+    """
     signal.signal(signal.SIGALRM, __handle_timeout)
     signal.setitimer(signal.ITIMER_REAL, worker.timeout)
-    t0 = time.time()
-
 
     data = []
-
+    # Load results sets
     for json_path in worker.json_path:
         with open(json_path) as f:
             data.append(json.load(f))
 
-    print('Loading Ground truth file: %s'%worker.ground_truth_path)
-    if worker.ground_truth_path is not None:
-        with open(worker.ground_truth_path) as f:
-
-            ground_truth = json.load(f)
-    else:
-        ground_truth = None
-
-    if(len(data) == 0):
+    if len(data) == 0:
         print("No data found for ", worker.json_path)
         return
 
     print("###### Running metrics against " + data[0]['job_name'])
-    #load the graph and ground truth in
-    #data_mod =  importlib.import_module('circulo.data.'+data[0]['dataset']+'.run')
-
-    # instance = None
-
-    # for name,cls in inspect.getmembers(data_mod):
-    #     if inspect.isclass(cls) and issubclass(cls, CirculoData) and name != "CirculoData":
-    #         instance = cls(data[0]['dataset'])
-
-    # if instance == None:
-    #     print("Unable to find data module for ", data[0]['dataset'])
-    #     return
-
-    # G = instance.get_graph()
-
-    # weights = 'weight' if G.is_weighted() else None
-    # #some datasets might not have ground truth
-    # try:
-    #     vc = instance.get_ground_truth(G)
-    #     ground_truth_cover = cover_from_membership( vc.membership, G)
-    # except Exception as e:
-    #     print("\t++NOTE for ", data[0]['dataset'], ": Ground Truth Not Available")
-    #     ground_truth_cover = None
-
-    # results_covers = []
-    # for i in range(len(data)):
-    #     results_covers.append(cover_from_membership(data['membership'], G))
-
     try:
-        # N2 cover
+        # Perform comparison of each result to every other results set
         metrics_matrix = np.zeros((len(data), len(data)))
         for i in range(len(data)-1):
             for j in range(i, len(data)):
                 # Diagonal is a comparison against ground truth
-                if (i == j):
-                    if ground_truth is not None:
-                        results = metrics.omega.omega_index(data[i]['membership'], ground_truth['membership'])
-                    else:
-                        results = 0
+                if i == j:
+                    # Omega score of results compared against itself should be 1
+                    results = 1
                 else:
-                    #metrics_matrix[i][j] = metrics.omega.omega_index(data[i]['membership'], data[j]['membership'])
                     results = (metrics.omega.omega_index(data[i]['membership'], data[j]['membership']))
 
                 if type(results) == int or type(results) == float or type(results) == np.float64:
-                    metrics_matrix[i][j]  = results
-                    metrics_matrix[j][i]  = results
+                    metrics_matrix[i][j] = results
+                    metrics_matrix[j][i] = results
                 else:
                     print(type(results), results)
-                    
-                
         
         algorithms = []
         name = None
@@ -176,23 +142,6 @@ def analyze_json(worker):
         print(e)
         traceback.print_exc(file=sys.stdout)
         return
-
-
-def cover_from_membership(membership, G):
-
-    if(membership is None):
-        return None
-
-    cluster_dict = {}
-
-    for vertex_id, cluster_id_list in enumerate(membership):
-        for cluster_id in cluster_id_list:
-            if(cluster_id not in cluster_dict):
-                cluster_dict[cluster_id] = []
-            cluster_dict[cluster_id].append(vertex_id)
-
-    return VertexCover(G, [v for v in cluster_dict.values()])
-
 
 if __name__ == "__main__":
     main()
