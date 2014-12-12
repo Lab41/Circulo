6a6ea7c9e0563798dead26cbc59ef6287638c05a
Yonas Tesfaye <ytesfaye@l41-lt-3.local>
2014-12-12 14:18:04 -0800

    Cleanup cluster_omega_comparison
    Several cleanup items:
    1) Improve data loading to take advantage of np.loadtext
    2) Change number of clusters to be configurable with a default guess of
    max(3, sqrt(n)/2)
    3) Add ability to print aggregations across datasets

--- experiments/cluster_omega_comparison.py
+++ experiments/cluster_omega_comparison.py
@@ -15,38 +15,33 @@
 # limitations under the License.
 import numpy as np
 from sklearn.cluster import spectral_clustering
+import argparse
+from math import floor, sqrt
+from operator import itemgetter
 import os
 import glob
 
 
-def run_distance_matrix_clustering(fname):
+def run_distance_matrix_clustering(fname, n=None):
     """
     Take a given input file (output of run_comparison.py which is a distance matrix of the results where Omega score
      is the distance metric) and use spectral clustering to figure out which algorithms are most similar in the results.
-     This is done by running spectral clustering for varying number of clusters and seeing how often two algorithms end
-      up in the same clsuter
 
     Args:
         fname: Input csv file. It
     """
 
     # Read in csv file. Parse header row and keep the rest for processing
-    lines = open(fname).read().strip().split('\n')
-    headerRow = lines[0].split(',')
-    lines = lines[1:]
-
-    # Create a list of lists of the distance matrix
-    pyMatrix = []
-    size = len(lines)  # Assume square for now
-    for line in lines:
-        pyMatrix.append([float(val) for val in line.split(',')])  # Assume comma delimited
-
-    # Turn into numpy array
-    npMatrix = np.matrix(pyMatrix)
+    f = open(fname)
+    # Read header row before passing to loadtxt
+    headerRow = f.readline().split(',')
+    npMatrix = np.loadtxt(f, delimiter=',')
+    size = len(npMatrix)
 
     # Set diagonals to 1
     for i in range(size):
         npMatrix[i, i] = 1
+
     # Spectral clustering doesn't like values less than 0.
     # TODO: Does it make sense to set values < 0 to be the abs(value)??? Seems better than just setting it to 0.
     #       Research meaning of an Omega score < 0
@@ -56,45 +51,63 @@ def run_distance_matrix_clustering(fname):
                 npMatrix[i,j] = abs(npMatrix[i,j])
 
     # Run spectral clustering for various n_clusters and keep track of how often any two algorithms end up in the same cluster
-    pairs = {}
     print('------------------------------------')
     print("Cluster Results for: ", os.path.basename(fname))
-    # Run clustering algorithms for various number of clusters
-    for n in range(3, size-1):
-        result = spectral_clustering(npMatrix, n_clusters=n)
 
-        # result is a list of
-        for i in range(max(result)):
-            algorithms = []
-            for j, val in enumerate(result):
-                if val == i:
-                    algorithms.append(headerRow[j])
+    if n is None:
+        # Set default value to sqrt(n)/2 if not specified by user
+        n = max(3, floor(sqrt(size)/2))
 
-            if len(algorithms) > 1:
-                algorithms.sort()
-                for x in range(len(algorithms)):
-                    for y in range(x+1,len(algorithms) ):
-                        pairName = algorithms[x] + '--' + algorithms[y]
-                        if pairName in pairs:
-                            pairs[pairName] += 1
-                        else:
-                            pairs[pairName] = 1
+    if n > size:
+        raise ValueError('N=%d is greater than the size (%d)'%(n, size))
 
-    # Sort on frequency and print
+    print("N=%d"%n)
+    results = []
+    result = spectral_clustering(npMatrix, n_clusters=n)
+    for i in range(max(result)):
+        algorithms = []
+        # Map clusters to names
+        for j, val in enumerate(result):
+            if val == i:
+                algorithms.append(headerRow[j])
+        # Print Clusters
+        print(algorithms)
+        results.append(algorithms)
+    return results
+
+def print_pairwise_counts(results):
+    """
+    Print pairwise counts across results sets of how often two algorithms appear in the same cluster
+    Args:
+     results: list of list of clusters
+    """
+    pairs = {}
+    for clusters in results:
+        for cluster in clusters:
+            if len(cluster) > 1:
+                cluster.sort()
+            for x in range(len(cluster)):
+                for y in range(x+1,len(cluster) ):
+                    pairName = cluster[x] + '--' + cluster[y]
+                    if pairName in pairs:
+                        pairs[pairName] += 1
+                    else:
+                        pairs[pairName] = 1
+
     tuples = []
     for pair in pairs:
-        tuples.append((pairs[pair], pair))
+        tuples.append([pair, pairs[pair]])
 
-    tuples.sort()
-    for (count, pair) in tuples:
-        # TODO: This should do something better than just printing to the screen
+    print("Printing Results Across Datasets")
+    for (pair, count) in sorted(tuples, key=itemgetter(1)):
         print(pair, count)
 
 
 def main():
     parser = argparse.ArgumentParser(description=
                                      'Use Relative Omega Scores to determine similarity of algorithms')
-    parser.add_argument('input_path', type=str, help='file or directory containing  metric json files')
+    parser.add_argument('input_path', type=str, help='file or directory containing  csv files')
+    parser.add_argument('--n', type=int, help='Number of clusters to look for')
     args = parser.parse_args()
 
     if not os.path.exists(args.input_path):
@@ -104,11 +117,17 @@ def main():
 
     if os.path.isdir(args.input_path):
         file_names = glob.glob(os.path.join(args.input_path, '*.csv'))
+        results = []
         for file_name in file_names:
-            run_distance_matrix_clustering(file_name)
+            results.append(run_distance_matrix_clustering(file_name, args.n))
+
+        print_pairwise_counts(results)
     else:
-        print("Path \"{}\" is not a directory".format(args.input_path))
-        return
+        if os.path.isfile(args.input_path):
+            run_distance_matrix_clustering(args.input_path, args.n)
+        else:
+            print("Invalid Path: %s"%args.input_path)
+            return
 
 if __name__ == "__main__":
     main()
